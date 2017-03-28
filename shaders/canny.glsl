15d4ee46fd524aa1f682f80df531d1a3430e5ec8
spoonsplz <modanhan@live.com>
2017-03-28 10:33:59 -0600

    better non max suppression

--- shaders/canny.glsl
+++ shaders/canny.glsl
@@ -19,43 +19,29 @@ void main(void)
 {
 	vec2 dv=textureSize(TextureImage, 0).xy;
     vec4 fc0 = vec4(texture(TextureImage, Textcoord).xyz, 1.0);
-    if(fc0.x+fc0.y+fc0.z<0.5)fc0.xyz=vec3(0);
+    if(fc0.x+fc0.y+fc0.z<3.9)fc0.xyz=vec3(0);
     {
     	vec4 tc=texture(TextureImage, Textcoord+vec2(1/dv.x,0/dv.y));
     	if(fc0.x+fc0.y+fc0.z<tc.x+tc.y+tc.z)fc0.xyz=vec3(0);
     }
-    {
-    	vec4 tc=texture(TextureImage, Textcoord+vec2(0/dv.x,1/dv.y));
-    	if(fc0.x+fc0.y+fc0.z<tc.x+tc.y+tc.z)fc0.xyz=vec3(0);
-    }
+
     {
     	vec4 tc=texture(TextureImage, Textcoord+vec2(-1/dv.x,0/dv.y));
     	if(fc0.x+fc0.y+fc0.z<tc.x+tc.y+tc.z)fc0.xyz=vec3(0);
     }
-    {
-    	vec4 tc=texture(TextureImage, Textcoord+vec2(0/dv.x,-1/dv.y));
-    	if(fc0.x+fc0.y+fc0.z<tc.x+tc.y+tc.z)fc0.xyz=vec3(0);
-    }
+
     
     vec4 fc1 = vec4(texture(TextureImage, Textcoord).xyz, 0.0);
-    if(fc1.x+fc1.y+fc1.z<0.5)fc1.xyz=vec3(0);
+    if(fc1.x+fc1.y+fc1.z<3.9)fc1.xyz=vec3(0);
     {
-    	vec4 tc=texture(TextureImage, Textcoord+vec2(1/dv.x,-1/dv.y));
+    	vec4 tc=texture(TextureImage, Textcoord+vec2(0/dv.x,1/dv.y));
     	if(fc1.x+fc1.y+fc1.z<tc.x+tc.y+tc.z)fc1.xyz=vec3(0);
     }
     {
-    	vec4 tc=texture(TextureImage, Textcoord+vec2(-1/dv.x,1/dv.y));
+    	vec4 tc=texture(TextureImage, Textcoord+vec2(0/dv.x,-1/dv.y));
     	if(fc1.x+fc1.y+fc1.z<tc.x+tc.y+tc.z)fc1.xyz=vec3(0);
     }
-    {
-    	vec4 tc=texture(TextureImage, Textcoord+vec2(-1/dv.x,-1/dv.y));
-    	if(fc1.x+fc1.y+fc1.z<tc.x+tc.y+tc.z)fc1.xyz=vec3(0);
-    }
-    {
-    	vec4 tc=texture(TextureImage, Textcoord+vec2(1/dv.x,1/dv.y));
-    	if(fc1.x+fc1.y+fc1.z<tc.x+tc.y+tc.z)fc1.xyz=vec3(0);
-    }
-    
+
     if((fc0+fc1).x>0&&(fc0+fc1).y>0&&(fc0+fc1).z>0)FragmentColour=vec4(1);
     else FragmentColour=vec4(0,0,0,1);
   //  FragmentColour=fc0+fc1;
