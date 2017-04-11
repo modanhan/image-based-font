6901d1248544062bdd676aabc0c6e5c872f3b0ed
spoonsplz <modanhan@live.com>
2017-04-11 12:29:00 -0600

    hope this works

--- bspline.cpp
+++ bspline.cpp
@@ -1,5 +1,8 @@
 #include "bspline.h"
 
+using namespace std;
+using namespace glm;
+
 float mix0(bspline* spline, int i, int n, float x){
 	if((spline->knot[i+n]-spline->knot[i])==0)return 0;
 	else return (x-spline->knot[i])/(spline->knot[i+n]-spline->knot[i]);
@@ -8,6 +11,52 @@ float mix0(bspline* spline, int i, int n, float x){
 float mix1(bspline* spline, int i, int n, float x){
 	if((spline->knot[i+n+1]-spline->knot[i+1])==0)return 0;
 	else return (spline->knot[i+n+1]-x)/(spline->knot[i+n+1]-spline->knot[i+1]);
+}
+
+// basis function fast
+float Bfast(bspline* spline, int k, int n, float x){
+	vector<float> v(n+1, 0);
+	
+	// v[i]=B(k+i,n,x)
+	for(int i=0;i<=n;i++){
+		if(spline->knot[i+k]<=x&&x<spline->knot[i+1+k])v[i]=1;
+	}
+	
+	for(int j=1;j<=n;j++){
+		vector<float> w(n+1,0);
+		for(int i=0;i<n;i++){
+			w[i]=mix0(spline, i+k,j,x)*v[i]+mix1(spline, i+k,j,x)*v[i+1];
+		}
+		v=w;
+	}
+	return v[0];
+}
+
+void initBSplineGeometry(bspline* spline){
+    vector<vec2> vertices;
+    vector<vec3> colors;
+    int delta=0;
+    for(float x=0;x<=1+spline->stepsize/2;x+=spline->stepsize){
+    	vec2 p(0,0);
+    	while(spline->knot[delta]<=x)delta++;
+    	for(int i=delta-spline->degree-1;i<delta;i++){
+    		p.x+=spline->point[i].x*Bfast(spline,i,spline->degree,x);
+    		p.y+=spline->point[i].y*Bfast(spline,i,spline->degree,x);
+    	}
+    	vertices.push_back(p);
+    }
+    
+    colors.assign(vertices.size(), vec3(1));
+    
+	spline->geometry.elementCount = vertices.size();
+
+	glBindBuffer(GL_ARRAY_BUFFER, spline->geometry.vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(vec2), &vertices[0], GL_STATIC_DRAW);
+		
+	glBindBuffer(GL_ARRAY_BUFFER, spline->geometry.colourBuffer);
+	glBufferData(GL_ARRAY_BUFFER, colors.size()*sizeof(vec3), &colors[0], GL_STATIC_DRAW);
+		
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void initBSpine(bspline* spline){
