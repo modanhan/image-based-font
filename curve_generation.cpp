0aecc622c582f37d4aacd3a0cebd8617ef1ffa47
spoonsplz <modanhan@live.com>
2017-04-18 17:28:38 -0600

    reverse chaikin subdivision

--- curve_generation.cpp
+++ curve_generation.cpp
@@ -9,7 +9,8 @@ using namespace glm;
 namespace curve_generation{
 	bspline bspline1;
 	graphics::MyShader shader;
-	
+
+	void generate_reverse_chaikin();
 	
 	vector<bspline> bsplines;
 
@@ -20,6 +21,9 @@ namespace curve_generation{
 	}
 	
 	void generate(){
+		generate_reverse_chaikin();
+		return;
+	
 		bsplines.resize(point_geometry::target_curves.size());
 		for(int i=0;i<point_geometry::target_curves.size();i++){
 			int tsize=point_geometry::target_curves[i].size()-1;
@@ -31,6 +35,54 @@ namespace curve_generation{
 				t.x-=1;
 				t.y-=1;
 				v.push_back(vec2(t.y,-t.x));
+			}
+			bsplines[i].point=v;
+			initBSpine(&bsplines[i]);
+		}
+	}
+	
+	vector<vec2> reverse_chaikin(vector<vec2>& ov){
+		if(ov.size()<6)return ov;
+		vector<vec2> v=ov;
+	
+		// preprocess
+		if(v.size()%2==0){
+			v[v.size()-2]=v.back();
+			v.pop_back();
+		}
+		
+		v[1]=v[0]+(v[1]-v[0])*vec2(4.0/3.0);
+		
+		for(int i=2;i<v.size()-1;i+=2){
+			vec2 vi=v[i];
+			vec2 vi1=v[i+1];
+			v[i]=vi+(vi-vi1)/vec2(2);
+			v[i+1]=vi1+(vi1-vi)/vec2(2);
+		}
+		
+		vector<vec2> ans;
+		ans.push_back(v[0]);
+		for(int i=1;i<v.size()-2;i+=2){
+			ans.push_back((v[i]+v[i+1])/vec2(2));
+		}
+		ans.push_back(v.back());
+		
+		return ans;
+	}
+	
+	void generate_reverse_chaikin(){
+		bsplines.resize(point_geometry::target_curves.size());
+		for(int i=0;i<point_geometry::target_curves.size();i++){
+			vector<vec2> v=point_geometry::target_curves[i];
+			for(int i=0;i<5;i++)v=reverse_chaikin(v);
+			for(int i=0;i<v.size();i++){
+				auto t=v[i];
+				t.x/=window_width/2;
+				t.y/=window_height/2;
+				t.x-=1;
+				t.y-=1;
+				v[i].x=t.y;
+				v[i].y=-t.x;
 			}
 			bsplines[i].point=v;
 			initBSpine(&bsplines[i]);
